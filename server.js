5c2884727cad23497582c09ef98bb43a9b01eac1
unknown <mmontano@SDIL-041B.SDI>
2015-10-14 11:43:54 -0400

    debugging query

--- server.js
+++ server.js
@@ -137,7 +137,7 @@ app.get('/listinglinks',function(req,res) {
      
      if (err) console.error(err);
         
-        var select = 'SELECT link_name__c, name, links__c FROM salesforce.SDI_Reps_Link__c WHERE name = $1 ';
+        var select = 'SELECT link_name__c, name, links__c FROM salesforce.SDI_Reps_Link__c WHERE name = $2 ';
         conn.query(select, function(err, result) {
           
            if (err) {
