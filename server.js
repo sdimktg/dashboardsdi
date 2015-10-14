675ab7d40b72f87e3462a0de7d49304b7c4182df
unknown <mmontano@SDIL-041B.SDI>
2015-10-14 17:43:40 -0400

    debuggin

--- server.js
+++ server.js
@@ -127,8 +127,8 @@ app.get('/listinglinks',function(req,res) {
      if (err) console.error(err);
          
          
-         var check = 'SELECT sfid, links__c, link_name__c FROM salesforce.SDI_Reps_Link__c WHERE relatedobject__c = a5i560000004CB2AAM ';
-            conn.query(check,[req.body.relatedobject__c],
+         var check = 'SELECT sfid, links__c, link_name__c FROM salesforce.SDI_Reps_Link__c WHERE relatedobject__c = $1 ';
+            conn.query(check,"a5i560000004CB2AAM",
             
             function(err, result){
                 done();
