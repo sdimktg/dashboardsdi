486d898467ac3a42f8fd44497ce795c4b525dc4d
unknown <mmontano@SDIL-041B.SDI>
2015-10-20 09:57:30 -0400

    changing interface datepicker

--- public/javascript/events.js
+++ public/javascript/events.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
     $('#datePicker')
         .datepicker({
-            format: 'mm/dd/yyyy'
+            format: 'mm/dd/yyyy',
+            pickerPosition: "bottom-left"
         })
         .on('changeDate', function(e) {
             // Revalidate the date field
