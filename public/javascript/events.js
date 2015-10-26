620164aaaf5881a9060ca42d80d1aa1190864b67
unknown <mmontano@SDIL-041B.SDI>
2015-10-26 10:23:54 -0400

    autocomplete test6

--- public/javascript/events.js
+++ public/javascript/events.js
@@ -41,8 +41,7 @@ Dropdown List State and Location(Test Mode)will  be filling with json object fro
         $.ajax({
           url: "/site_location",   
           dataType: "jsonp",
-          type: "POST",
-          data: {q: request.term},
+          data: {q: request.term}
             
             //JSON.stringify({ "full_location__c": full_location__c }),  
             
