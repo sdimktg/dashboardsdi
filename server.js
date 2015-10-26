4141fb692a1b5fc8d840f89eb54bc4060035f75d
unknown <mmontano@SDIL-041B.SDI>
2015-10-26 12:42:06 -0400

    testing

--- server.js
+++ server.js
@@ -159,7 +159,7 @@ app.get('/site_location',function(req,res) {
      if (err) console.error(err);
         
         var select = 'SELECT  full_location__c FROM salesforce.SDI_Site_Location__c WHERE store__c = $1 ';
-        conn.query(select,[req.body.store__c], function(err, result) {
+        conn.query(select,[req.body.q], function(err, result) {
           
            if (err) {
                
