892ea5f2cfca8e7c975236c924f165965b0de333
unknown <mmontano@SDIL-041B.SDI>
2015-10-26 14:11:01 -0400

    testing

--- public/javascript/events.js
+++ public/javascript/events.js
@@ -7,7 +7,7 @@ Dropdown List State and Location(Test Mode)will  be filling with json object fro
 ******************************************************************************************************/
     
     $('#test').focusout(function(){
-        
+        location.reload();
         var request = this.value;
         
         alert(request);
